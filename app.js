a3c96a3888acfdd861dd5cfc8052262068084aa9
Rosalio404 <Rosalio404@protonmail.com>
2020-10-03 22:30:09 -0500

    Add boilerplate, npm installs

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-//jshint esversion:6
+////// Modules
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+////// Variables
+const port = 3000;
+
+////// Express
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+// GET
+app.get("/", function (req, res) {
+		res.render("home");
+});
+
+app.get("/login", function (req, res) {
+		res.render("login");
+});
+
+app.get("/register", function (req, res) {
+		res.render("register");
+});
+
+// Listen
+app.listen(port, function () {
+		console.log("Server started on port " + port);
+});
