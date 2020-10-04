bcf24a565c192a67b537e57070d50800c0305f14
Rosalio404 <Rosalio404@protonmail.com>
2020-10-04 15:28:48 -0500

    Add dotenv/Environmental variables

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 ////// Modules
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
@@ -18,8 +19,7 @@ const userSchema = new mongoose.Schema({
 	password: String
 });
 // Encryption
-const secret = "Heresyourlongstring";
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 // Models
 const User = new mongoose.model("User", userSchema);
 ////// Express
