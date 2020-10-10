85d985aafccd98a0020d2cabd7627da4ae82e07e
Rosalio404 <Rosalio404@protonmail.com>
2020-10-10 12:55:59 -0500

    Finishing touches I don't remember..

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ const userSchema = new mongoose.Schema({
 	email: String,
 	password: String,
 	googleId: String,
+	secret: String,
 });
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
@@ -132,8 +133,21 @@ app.get("/register", function (req, res) {
 });
 
 app.get("/secrets", function (req, res) {
+	User.find({"secret": {$ne: null}}, function (err, foundUsers) {
+		if (!err) {
+			if (foundUsers) {
+				console.log(foundUsers);
+				res.render("secrets", {usersWithSecrets: foundUsers});
+			}
+		} else {
+			res.send(err);
+		}
+	});
+});
+
+app.get("/submit", function (req, res) {
 	if (req.isAuthenticated()) {
-		res.render("secrets");
+		res.render("submit");
 	} else {
 		res.redirect("/login");
 	}
@@ -167,6 +181,23 @@ app.post("/login", function (req, res) {
 			res.send(err);
 		}
 	});
+});
+
+app.post("/submit", function (req, res) {
+	const submittedSecret = req.body.secret;
+	User.findById(req.user.id, function (err, foundUser) {
+		if (!err) {
+			if (foundUser) {
+				foundUser.secret = submittedSecret;
+				foundUser.save(function () {
+					res.redirect("/secrets");
+				});
+			}
+		} else {
+			res.send(err);
+		}
+	});
+
 });
 
 // Listen
